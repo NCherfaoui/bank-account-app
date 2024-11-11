200a38fc29d5cd291c68cbfde4922c5f363e50de
Nassim Cherfaoui <99206668+NCherfaoui@users.noreply.github.com>
2024-11-11 14:32:13 +0100

    OpenFeign added to AccountService
    Signed-off-by: Nassim Cherfaoui <99206668+NCherfaoui@users.noreply.github.com>

--- account-service/src/main/java/com/nch/accountservice/web/AccountRestController.java
+++ account-service/src/main/java/com/nch/accountservice/web/AccountRestController.java
@@ -1,10 +1,11 @@
 package com.nch.accountservice.web;
 
+import com.nch.accountservice.clients.CustomerRestClient;
 import com.nch.accountservice.entities.BankAccount;
+import com.nch.accountservice.model.Customer;
 import com.nch.accountservice.repository.BankAccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,18 +13,23 @@ import java.util.List;
 @RestController
 //@RequestMapping("/api")
 public class AccountRestController {
-    private BankAccountRepository accountRepository;
+    private final BankAccountRepository accountRepository;
+    private final CustomerRestClient customerRestClient;
 
-    public AccountRestController (BankAccountRepository accountRepository) {
+    public AccountRestController (BankAccountRepository accountRepository, CustomerRestClient customerRestClient) {
         this.accountRepository = accountRepository;
+        this.customerRestClient = customerRestClient;
     }
     @GetMapping("/accounts")
     public List<BankAccount> accountList() {
         return accountRepository.findAll();
     }
 
-    @GetMapping("/account/{id}")
-    public BankAccount getAccount(@PathVariable String id) {
-        return accountRepository.findById(id).get();
+    @GetMapping("/accounts/{id}")
+    public BankAccount bankAccount(@PathVariable String id) {
+        BankAccount bankAccount = accountRepository.findById(id).get();
+        Customer customer = customerRestClient.findCustomerById(bankAccount.getCustomerId());
+        bankAccount.setCustomer(customer);
+        return bankAccount;
     }
 }
